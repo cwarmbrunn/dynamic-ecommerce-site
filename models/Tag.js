d25c26cceb4eeec051b1c88bb924a2c6b5c7f048
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-04-29 14:39:52 -0400

    added in tag model

--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,37 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // Columns Go Here! //
+
+    // Column #1 - ID //
+    id: {
+      // Data Type - Set this to integer
+      type: DataTypes.INTEGER,
+      // Does NOT allow null
+      allowNull: false,
+      // Set as primary key
+      primaryKey: true,
+      // Uses auto increment
+      autoIncrement: true,
+    },
+
+    // Column #2 - Tag Name //
+    tag_name: {
+      // Data Type - Set this to string
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
