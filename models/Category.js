41652729364d9928f672d76148e1b9378651f492
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-04-29 12:07:25 -0400

    added and tested connection

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,34 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Columns Go Here! //
+
+    // Column #1 - ID //
+    // Set the column name to ID
+    id: {
+      // Type - Set this to integer
+      type: DataTypes.INTEGER,
+      // Does NOT allow null variables
+      allowNull: false,
+      // Set as primary key
+      primaryKey: true,
+      // Uses auto increment
+      autoIncrement: true,
+    },
   },
+
+  // Sequelize //
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
