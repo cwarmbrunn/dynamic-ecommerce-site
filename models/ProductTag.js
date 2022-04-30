9ef44dce83ba30c975b4ae33568e7ba2d6aaec74
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-04-30 11:32:26 -0400

    addedfinal touches to productTag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,34 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // Columns Go Here! //
+
+    // Column #1 - ID //
+    id: {
+      // Data Type - Set this to integer
+      type: DataTypes.INTEGER,
+      // Does NOT allow null
+      allowNull: false,
+      // Set as primary key
+      primaryKey: true,
+      // Uses auto increment
+      autoIncrement: true,
+    },
+    product_id: {
+      // Data Type - Set this to integer
+      type: DataTypes.INTEGER,
+      // References the product model's id
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
